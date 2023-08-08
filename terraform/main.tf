42b18d4c7891308f7b664ee6d960049632bd42cd
Darst King <41591910+sking8484@users.noreply.github.com>
2023-08-07 22:31:41 -0400

    Switching over to ec2 instead of kubernetes. Creating vpc, subnet, igw, security group and ec2 instance. Bootstrapping EC2 instance with running image (#7)

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,104 +5,91 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "Desk1DatamartCluster"
-}
-
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-
-  name = "education-vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
-  }
-}
-
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
-
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
-
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-  }
-}
-
-
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+resource "aws_vpc" "datamart_vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
+    Name = "datamart-vpc"
+  }
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+
+resource "aws_subnet" "datamart_subnet" {
+  vpc_id = aws_vpc.datamart_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "primary_subnet"
   }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  vpc_id = aws_vpc.datamart_vpc.id
+
+  ingress {
+    from_port =0 
+    to_port=0 
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_internet_gateway" "datamart_gw" {
+  vpc_id = aws_vpc.datamart_vpc.id
+}
+
+resource "aws_route_table" "datamart_routetable" {
+  vpc_id = aws_vpc.datamart_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.datamart_gw.id 
+  }
+}
+
+resource "aws_route_table_association" "subnet-association" {
+  subnet_id = aws_subnet.datamart_subnet.id
+  route_table_id = aws_route_table.datamart_routetable.id
+}
+
+resource "aws_instance" "foo" {
+  key_name = "datamart_key"
+  ami = "ami-024e6efaf93d85776"
+  instance_type = "t2.micro"
+
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  subnet_id = aws_subnet.datamart_subnet.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  associate_public_ip_address = true
+  user_data = <<EOF
+#! /bin/bash
+sudo apt update -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" -y
+sudo apt update -y
+sudo apt-get install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+sudo docker pull lebesgel/desk1_datamart
+sudo docker run -p 80:8080 -d lebesgel/desk1_datamart
+EOF
+}
+
+
